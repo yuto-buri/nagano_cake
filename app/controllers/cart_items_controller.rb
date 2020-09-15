83591a4e4bb39e4aab88c73ca97eca7281bd5dd8
motokitakumi <mtktkm242424@gmail.com>
2020-09-15 21:27:05 +0900

    [update]

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,4 +1,30 @@
 class CartItemsController < ApplicationController
   def index
+  	cart_items = CartItem.all
+  end
+
+  def update
+  	cart_item = CartItem.find(params[:id])
+  	cart_item.update(cart_item_params)
+  end
+
+  def destroy
+  	cart_item = CartItem.find(params[:id])
+  	cart_item.destroy
+  	redirect_to
+  end
+
+  def destroy_all
+  end
+
+  def create
+  	@cart_item = CartItem.new(cart_item_params)
+  	@cart_item.save
+  	redirect_to
+  end
+
+  private
+  def cart_item_params
+  	params.require(:cart_item).permit(:item_id,:customer_id,:amount)
   end
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,8 +3,20 @@ class CustomersController < ApplicationController
   end
 
   def show
+  	customer = Customer.find(params[:id])
   end
 
   def withdraw
   end
+
+  def update
+  	customer = Customer.find(params[:id])
+  	customer.update
+  	redirect_to
+  end
+
+  def unsubscribe
+  end
+
+
 end
