93452a3e42d9b60e39b7141ff6936096cf53417f
vagrant <vagrant@localhost.localdomain>
2020-09-16 09:35:26 +0000

    custommer_new

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -52,7 +52,7 @@ class Customers::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
-   customer_path(resource)
+   items_index_path
    end
 
   # The path used after sign up for inactive accounts.
