7e274a0ecb81ed895607ccdd63bfd36f4029a6d8
motokitakumi <mtktkm242424@gmail.com>
2020-09-16 13:46:36 +0900

    nothing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
   	private
   	def configure_permitted_parameters
-    	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:is_deleted :first_name :first_name_kana :last_name :last_name_kana :postal_code :address :telephone_number :email])
   	end
 
   	def after_sign_in_path_for(resource)
