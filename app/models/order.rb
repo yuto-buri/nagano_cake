c8e3c8cf77e0e8e10e5a749622d2444e7fefc7c1
motokitakumi <mtktkm242424@gmail.com>
2020-09-20 13:32:53 +0900

    modify motoki.takumi

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,22 @@
 class Order < ApplicationRecord
 	belongs_to :customer
+	enum payment: {
+		creditcard:0,
+		transfer:1
+	}
+	
+	has_many :order_items, dependent: :destroy
+	#中間テーブルを介して複数のプロダクトを持つ
+	has_many :products, :through => :order_items
+	#オーダーアイテムを複数追加するためのメソッド
+	accepts_nested_attributes_for :order_items
 
+	belongs_to :customer
+
+	validates :addressee, presence: true,
+	length: {maximum: 35, minimum: 2}
+	validates :post_code, presence: true,
+	length: {maximum: 10, minimum: 2}
+	validates :send_to_address, presence: true,
+	length: {maximum: 50, minimum: 3}
 end
