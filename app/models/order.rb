1f9fadd9f03adaf6a0335636719433644637b550
motokitakumi <mtktkm242424@gmail.com>
2020-09-21 16:33:13 +0900

    add order comfirm

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,14 @@
 class Order < ApplicationRecord
 	belongs_to :customer
-	enum payment: {
-		creditcard:0,
-		transfer:1
-	}
+	enum payment: {creditcard: 0, transfer: 1}
 
+	enum status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
 
 	has_many :order_items, dependent: :destroy
 	#中間テーブルを介して複数のプロダクトを持つ
 	has_many :items, :through => :order_items
 	#オーダーアイテムを複数追加するためのメソッド
 	accepts_nested_attributes_for :order_items
-
-	belongs_to :customer
 
 	validates :addressee, presence: true,
 	length: {maximum: 35, minimum: 2}
@@ -21,3 +17,4 @@ class Order < ApplicationRecord
 	validates :send_to_address, presence: true,
 	length: {maximum: 50, minimum: 3}
 end
+
