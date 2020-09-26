0174aee8f093cf5d36b050866239d04a71dad8cb
motokitakumi <mtktkm242424@gmail.com>
2020-09-26 15:24:25 +0900

    delete file2

--- app/controllers/admins/order_details_controller 2.rb
+++
@@ -1,2 +0,0 @@
-class Admins::OrderDetailsController < ApplicationController
-end
--- app/controllers/admins/orders_controller 2.rb
+++
@@ -1,7 +0,0 @@
-class Admins::OrdersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-end
