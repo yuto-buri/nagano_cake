3ff245429546f58638c72abe40d61e5757fde384
vagrant <vagrant@localhost.localdomain>
2020-09-27 06:07:05 +0000

    add_layout

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -7,7 +7,7 @@ class Admins::OrdersController < ApplicationController
       @orders = Customer.find(params[:id]).orders.page(params[:page]).per(10)
 
     elsif request.fullpath.include? "today" # TOP本日受注分から来た場合
-      @orders = Order.where(created_at:  Time.zone.now.all_day).page(params[:page]).per(10)
+      @orders = Order.where(created_at: Time.zone.now.all_day).page(params[:page]).per(10)
 
     elsif request.fullpath.include? "yesterday" # TOP本日製作分から来た場合
       @orders = Order.where(created_at: 1.day.ago.all_day).page(params[:page]).per(10)
